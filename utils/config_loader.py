ffa9e90ead9b6d26d84b8fc3167a3344f4993d99
Sayed Ashfaq <302syedashfaq@gmail.com>
2025-08-10 18:35:39 +0530

    utils_executed

--- utils/config_loader.py
+++ utils/config_loader.py
@@ -1,7 +1,12 @@
 import yaml
+from pathlib import Path
 
 
-def load_config(config_path: str = "..\config\config.yaml") -> dict:
-    with open(config_path, "r") as file:
+def load_config(config_path: str = "C:\\Users\\302sy\\Desktop\\Generative AI\\StockSnapAI\\utils\\config_loader.py") -> dict:
+    with open(Path(config_path), "r") as file:
         config=yaml.safe_load(file)
     return config
+
+if __name__ == "__main__":
+    testing_config = load_config()
+    print(testing_config)
