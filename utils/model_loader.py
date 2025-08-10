47c92f764fc1423b78086d5f0037930daa2f99c2
Sayed Ashfaq <302syedashfaq@gmail.com>
2025-08-10 20:59:18 +0530

    Testing stocknews

--- utils/model_loader.py
+++ utils/model_loader.py
@@ -3,7 +3,7 @@ import sys
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from openai.types import embedding_model
-from utilities.config_loader import load_config
+from utils.config_loader import load_config
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_groq import ChatGroq
